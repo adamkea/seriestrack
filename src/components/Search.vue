d06dc612f008b17e68da82d3648c434ae5276938
Adam <adam.kearney@openapp.ie>
2019-05-02 19:29:13 -0400

    progress on show search

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -2,7 +2,35 @@
     <div class="flex-center">
         <div>
             <h2 class="text-center">Find Shows</h2>
-            <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+            <input class="form-control" type="text" placeholder="Search" aria-label="Search" v-model="search" v-on:keyup.enter="searchShow(search)">
+            <div v-if="results">
+                <div v-for="result in results" :key="result.imdbID">
+                    <div class="card-body">
+                        <h5 class="card-text">{{result.Title}}</h5>
+                    </div>
+                </div>
+            </div> 
         </div>
-    </div>
+      </div>
 </template>
+
+<script>
+import axios from 'axios';
+export default{
+    data(){
+        return{
+            results:[],
+            search:"",
+            'apikey': process.env.VUE_APP_API_KEY
+        }
+    },
+    methods:{
+        searchShow(query){
+            axios
+                .get('http://www.omdbapi.com/?s=' + query + '&apikey=' + this.apikey + '&type=series')
+                .then(response => this.results = response.data.Search)
+        }
+    }
+}
+
+</script>
